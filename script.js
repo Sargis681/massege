03e92b0cbb767741f37c1d6aa97f75cbd0b29567
unknown <sargismanukyan1298@gmail.com>
2025-05-31 14:58:03 +0400

    no speck

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const voice = document.getElementById("voice");
 envelope.addEventListener("click", () => {
   envelope.classList.toggle("open");
 
-  if (envelope.classList.contains("open")) {
-    // 🔊 Открытие — продолжаем проигрывать с того места
-    voice.play();
-  } else {
-    // ⏸ Закрытие — просто ставим на паузу, не сбрасываем
-    voice.pause();
-  }
+//   if (envelope.classList.contains("open")) {
+//     // 🔊 Открытие — продолжаем проигрывать с того места
+//     voice.play();
+//   } else {
+//     // ⏸ Закрытие — просто ставим на паузу, не сбрасываем
+//     voice.pause();
+//   }
 });
